1dab4c48da9153de191d163ed9ffb112b11cd2eb
sedx876 <swatkins876@gmail.com>
2020-08-06 09:13:37 -0400

    Show and Index Pages

--- instaclone/app/controllers/pics_controller.rb
+++ instaclone/app/controllers/pics_controller.rb
@@ -1,6 +1,10 @@
 class PicsController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index 
+  end
+
+  def show 
   end
 
   def new
@@ -9,6 +13,12 @@ class PicsController < ApplicationController
 
   def create
     @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: "YaY Your Pic Was Posted!!"
+    else
+      render 'new'
+    end
   end
 
 
@@ -17,6 +27,10 @@ class PicsController < ApplicationController
 
   def pic_params
     params.require(:pic).permit(:title, :description)
+  end
+
+  def find_pic
+    @pic = Pic.find(params[:id])
   end
 
 end
